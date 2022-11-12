9e4242139995731b5e42c6a377f48aaa3ce15427
Pawel Zguda <zgudapawel@gmail.com>
2022-11-12 18:28:27 +0100

    cleaning code, removing comments

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ const year = document.querySelector('.footer__copyright-year');
 const nameMobileContainer = document.querySelector('.name-mobile');
 const aboutSkills = document.querySelector('.about');
 const scrollToTop = document.querySelector('.scroll-top');
-
 const isMobile = window.matchMedia('(max-width: 1024px)').matches;
 
 for (let i = 0; i < 369; i++) {
@@ -30,30 +29,25 @@ for (let i = 0; i < 369; i++) {
     79, 80, 118, 119, 159, 160, 161, 162, 200, 201, 202, 203, 241, 242, 282,
     283, 284, 285, 323, 324, 325, 326,
   ];
-
   const el = document.createElement('div');
   el.classList = list.includes(i) ? 'box active' : 'box';
   boxContainer.appendChild(el);
 }
-
 window.addEventListener('scroll', () => {
   const offsetY = window.scrollY;
   const scrollTop = aboutSkills.offsetTop;
   const isSectionReached = offsetY > scrollTop;
-
   welcomeImgContainer.style.transform = `translate(${offsetY * 0.4}px , ${
     offsetY * 0.8
   }px)`;
 
   if (!isMobile) {
     welcomeTextContainer.style.transform = `translateY(${offsetY * 0.2}px)`;
-
     authorName.style.transform = `translateX(${offsetY * 0.1}px)`;
     jobTitleContainers[0].style.backgroundPositionY = `${offsetY * 0.6}px`;
     jobTitles[0].style.transform = `translateX(calc(202vh - ${offsetY}px))`;
     jobTitleContainers[1].style.backgroundPositionY = `${-offsetY * 0.5}px`;
     jobTitles[1].style.transform = `translateX(calc(-300vh + ${offsetY}px))`;
-
     projectsTitle.style.transform = `translateY(calc(510vh - ${offsetY}px))`;
     phones[0].style.transform = `translateX(calc(600vh - ${offsetY}px))`;
     laptop.style.transform = `translateX(calc(-730vh + ${offsetY}px))`;
